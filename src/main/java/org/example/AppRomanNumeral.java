7fc02a6baa71b6f59540559e10a79e89fe3afaa0
Fandikoos <danfandos@gmail.com>
2023-03-08 17:56:42 +0100

    GREEN: se implementa la funcionalidad mínima para superar los tests

--- src/main/java/org/example/AppRomanNumeral.java
+++ src/main/java/org/example/AppRomanNumeral.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class AppRomanNumeral {
     public String convertToRomanNumeral(int i) {
+        if (i == 2){
+            return "II";
+        }
         return "I";
     }
 }
