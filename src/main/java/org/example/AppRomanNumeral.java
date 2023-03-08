ecdc5f14e9682ac1e70d570d689686a9d68fd539
Fandikoos <danfandos@gmail.com>
2023-03-08 18:41:24 +0100

    Refactorizamos el codigo para poder gestionar de mejor manera todos los numeros decimales que pueden relacionarse con la Sigla romana V

--- src/main/java/org/example/AppRomanNumeral.java
+++ src/main/java/org/example/AppRomanNumeral.java
@@ -3,14 +3,15 @@ package org.example;
 public class AppRomanNumeral {
     public String convertToRomanNumeral(int numDecimal) {
         String romanNumber = "";
-        if (numDecimal == 7){
-            return "VII";
-        }
-        if (numDecimal == 6){
-            return "VI";
-        }
-        if (numDecimal == 5){
-            return "V";
+
+        /* Si el decimal es mayor que 5, viene a este if, entonces se suma al string
+        * la sigla romana V y luego le restamos al decimal 5 para que pueda seguir progresanod
+        * en el siguiente array y que pueda sumar la sigla I en función de lo que se necesite
+        * para transoformar el número*/
+        
+        if (numDecimal >= 5){
+            romanNumber += "V";
+            numDecimal -= 5;
         }
 
         //Recorremos un array que nos permitira sacar los 3 primeros años
